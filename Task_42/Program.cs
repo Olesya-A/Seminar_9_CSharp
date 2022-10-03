266ca172bb290881d05b8ad132abc03ca5c20b47
Olesya <lesusha1@yandex.ru>
2022-10-04 01:27:39 +0300

    add Task_42

---
+++ Task_42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 42 Перевести число из десятичной системы в двоичную (рекурсия)
+
+Console.Write("Input integer number A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ConvertDecToBinaryRec(A));
+
+string ConvertDecToBinaryRec(int A)
+{
+      if (A == 0)
+      return string.Empty;
+
+      return ($"{ConvertDecToBinaryRec(A / 2)}{A % 2}");
+}
